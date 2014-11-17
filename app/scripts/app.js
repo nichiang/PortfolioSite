137ab568261de301c625a2b8b60ec25ebe695fcf
nichiang <nick.chiang@gmail.com>
2014-11-16 22:26:58 -0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolioSiteApp
+ * @description
+ * # portfolioSiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portfolioSiteApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
