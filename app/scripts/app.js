73d38a9b558e9f16298960f982cc11c3ba69f38f
nichiang <nick.chiang@gmail.com>
2014-11-17 17:40:10 -0800

    website framework setup, first draft of content

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,62 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name portfolioSiteApp
- * @description
- * # portfolioSiteApp
- *
- * Main module of the application.
- */
-angular
-  .module('portfolioSiteApp', [
-    'ngAnimate',
-    'ngTouch'
-  ]);
+(function(){
+
+  var app = angular.module('portfolioSiteApp', ['ngAnimate', 'ngRoute', 'ngTouch']);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/touch_hover', {
+        templateUrl: 'views/project.html',
+        controller: 'ProjectCtrl',
+        controllerAs: 'projectCtrl'
+      })
+      .when('/ping_app', {
+        templateUrl: 'views/project.html',
+        controller: 'ProjectCtrl',
+        controllerAs: 'projectCtrl'
+      })
+      .when('/adaptive_ux', {
+        templateUrl: 'views/project.html',
+        controller: 'ProjectCtrl',
+        controllerAs: 'projectCtrl'
+      })
+      .when('/canvas_interactions', {
+        templateUrl: 'views/project.html',
+        controller: 'ProjectCtrl',
+        controllerAs: 'projectCtrl'
+      })
+      .when('/ppi_guidelines', {
+        templateUrl: 'views/project.html',
+        controller: 'ProjectCtrl',
+        controllerAs: 'projectCtrl'
+      });
+  });
+
+  app.controller('MainCtrl', ['$rootScope', '$http', function ($rootScope, $http) {
+    $rootScope.projects = [];
+
+    $http.get('views/projects.json').success(function(data) {
+      $rootScope.projects = data;
+    });
+  }]);
+
+  app.controller('ProjectCtrl', ['$rootScope', '$location', function($rootScope, $location) {
+    console.log($location.path());
+
+    this.currentProject = $rootScope.projects[$location.path().substring(1)];
+
+    this.hasIssues = function() {
+      return this.currentProject.issues !== null && this.currentProject.issues.length > 0;
+    };
+
+    this.hasApproach = function() {
+      return this.currentProject.approach !== null && this.currentProject.approach.length > 0;
+    };
+  }]);
+
+})();
